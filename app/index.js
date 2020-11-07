0c9cc58ce7873bd474b7cf730f2451560abb4cac
Wendy Dherin <wendydherin@gmail.com>
2020-11-07 16:34:31 +0100

    Add nav and toggle theme with context.

--- app/index.js
+++ app/index.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
 import './index.css'
 
 class App extends React.Component {
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
+    }
+  }
+
   render() {
-    return <div>Hello world!</div>
+    return (
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className='container'>
+              <Nav />
+            </div>
+          </div>
+        </ThemeProvider>
+      </Router>
+
+    )
   }
 }
 
