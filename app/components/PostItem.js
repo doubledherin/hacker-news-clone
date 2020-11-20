ecd1713e0ee3f656433ba846a5d7a7d8ef195828
Wendy Dherin <wendydherin@gmail.com>
2020-11-20 11:17:16 +0100

    Make PostItem functional.

--- app/components/PostItem.js
+++ app/components/PostItem.js
@@ -5,24 +5,19 @@ import CommentLink from './CommentLink'
 import UserLink from './UserLink'
 import { getDate, getTime } from '../utils/helpers'
 
-export default class PostItem extends React.Component {
+export default function PostItem ({ post }) {
+  const { by, time, title } = post
+  const dateString = getDate(time)
+  const timeString = getTime(time)
 
-  render() {
-    const { post } = this.props
-    const { by, kids, time, title } = post
-    const dateString = getDate(time)
-    const timeString = getTime(time)
-
-    return (
-      <React.Fragment> 
-        <li className='post'>
-          <a className='link' href={post.url}>{title}</a>
-          <div className='meta-info-light'>
-            by <UserLink userName={by} /> on {dateString}, {timeString} with <CommentLink post={post} /> comments
-          </div>
-        </li>
-      </React.Fragment>
-    )
-
-  }
+  return (
+    <React.Fragment> 
+      <li className='post'>
+        <a className='link' href={post.url}>{title}</a>
+        <div className='meta-info-light'>
+          by <UserLink userName={by} /> on {dateString}, {timeString} with <CommentLink post={post} /> comments
+        </div>
+      </li>
+    </React.Fragment>
+  )
 }
