affd6abed1f8d1055fedf5d64f993be869acd72d
Wendy Dherin <wendydherin@gmail.com>
2020-11-20 11:14:49 +0100

    Make Loading.js functional.

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -12,37 +12,31 @@ const styles = {
   }
 }
 
-class Loading extends Component {
+export default function Loading ({ text, speed }) {
 
-  state = {
-    content: this.props.text
-  }
-
-  componentDidMount() {
-    const { text, speed } = this.props
-    this.interval = window.setInterval(() => (
-      this.state.content === text + '...' ? this.setState({ content: text}) : this.setState(({ content }) => ({ content: content + '.' })
-    )) , speed)
-  }
-
-  componentWillUnmount() {
-    window.clearInterval(this.interval)
-  }
-  render() {
-    return (
-      <p style={styles.content}>{this.state.content}</p>
-    );
-  }
+  const [ content, setContent ] = React.useState(text)
+  
+  React.useEffect(
+    () => {
+      const id = window.setInterval(() => 
+        setContent(content => content === `${text}...` ? text : `${content}.`),
+        speed
+      )
+      return () => window.clearInterval(id)
+    }
+    , [text]
+  )
+  return (
+    <p style={styles.content}>{content}</p>
+  )
 }
 
 Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired
+  text: PropTypes.string,
+  speed: PropTypes.number
 }
 
 Loading.defaultProps = {
   text: 'Loading',
   speed: 300
 }
-
-export default Loading;
